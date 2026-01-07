9c1450831e940600001fcaa486c45da54e46c25a
learnmoreyoucandoit-hue <learnmoreyoucandoit@gmail.com>
2026-01-07 09:58:01 +0545

    Add files via upload

---
+++ .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a =10, b=5;
+    float c =5.5;
+    printf("sum of the values is %d\n",a+b);
+    printf ("tarka dhanuk\n");
+    printf ("value of a =%d \n value of c=%.3f", a,c);
+    //printf ("value of b=%.3f", b);
+
+    return 0;
+}
